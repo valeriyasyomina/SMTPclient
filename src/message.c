164b297d82f3df3e3928428cdc318feb2ee892ee
lera <valeriya.syomina@yandex.ru>
2017-12-30 13:54:30 +0300

    message struct changed

--- src/message.c
+++ src/message.c
@@ -1,5 +1,6 @@
 #include "message.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct message* create_empty_message()
 {
@@ -7,9 +8,59 @@ struct message* create_empty_message()
 	if (!message)
 		return NULL;
 	message->data = NULL;
-	message->headers = create_empty_message_headers_list();
+    message->id = NULL;
+    message->headers = NULL;
+    // message->headers = create_empty_message_headers_list();
 	return message;
 }
+
+struct message* create_message_width_data(char* data)
+{
+    if (!data)
+        return NULL;
+    struct message* message = (struct message*) malloc(sizeof(struct message));
+    if (!message)
+        return NULL;
+    int data_size = strlen(data);
+    message->data = (char*) malloc(data_size + 1);
+    strncpy(message->data, data, data_size);
+    message->data[data_size] = '\0';
+    message->headers = NULL;
+    return message;
+}
+
+void set_message_data(struct message* message, char* data)
+{
+    if (message && data)
+    {
+        if (message->data)
+        {
+            free(message->data);
+            message->data = NULL;
+        }
+        int data_size = strlen(data);
+        message->data = (char*) malloc(data_size + 1);
+        strncpy(message->data, data, data_size);
+        message->data[data_size] = '\0';
+    }
+}
+
+void set_message_id(struct message* message, char* id)
+{
+    if (message && id)
+    {
+        if (message->id)
+        {
+            free(message->id);
+            message->id = NULL;
+        }
+        int id_size = strlen(id);
+        message->id = (char*) malloc(id_size + 1);
+        strncpy(message->id, id, id_size);
+        message->id[id_size] = '\0';
+    }
+}
+
 
 void free_message(struct message* message)
 {
@@ -19,19 +70,41 @@ void free_message(struct message* message)
 		{
 			free(message->data);
 			message->data = NULL;
-		}
+        }
+        if (message->id)
+        {
+            free(message->id);
+            message->id = NULL;
+        }
 		free_message_headers_list(message->headers);
+        message->headers = NULL;
 	}
 }
 
-struct message* add_header(struct message* message, struct message_header* header)
+struct message* add_header_to_message(struct message* message, struct message_header* header)
 {
-	if (!message)
-		return NULL;
-	if (!header)
-		return NULL;
-	message->headers = add_header_to_list(message->headers, header);
-	if (!message->headers)
-		return NULL;
-	return message;
+    if (!message)
+        return NULL;
+    if (!header)
+        return NULL;
+
+    struct message_headers_list* header_item = create_message_headers_list_item(header);
+    message->headers = add_to_message_headers_list(message->headers, header_item);
+    return message;
 }
+
+int compare_message_id(struct message* message, char* id)
+{
+    if (!message || !message->id || !id)
+        return NULL_DATA;
+    return strcmp(message->id, id);
+}
+
+struct message_header* get_message_header_by_key(struct message* message, char* header_key)
+{
+    if (!header_key)
+        return NULL;
+    return get_header_by_key(message->headers, header_key);
+}
+
+
--- src/message.h
+++ src/message.h
@@ -1,13 +1,27 @@
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
 #include "message_headers_list.h"
 
 #define MAX_DATA_SIZE 2048
+#define NULL_DATA -1
+#define HAS_HEADER 0
+#define NOT_HAS_HEADER 1
 
 struct message
 {
 	char* data;
+    char* id;
 	struct message_headers_list* headers;
 };
 
 struct message* create_empty_message();
+struct message* create_message_width_data(char* data);
+void set_message_data(struct message* message, char* data);
+void set_message_id(struct message* message, char* id);
 void free_message(struct message* message);
-struct message* add_header(struct message* message, struct message_header* header);
+struct message* add_header_to_message(struct message* message, struct message_header* header);
+int compare_message_id(struct message* message, char* id);
+struct message_header* get_message_header_by_key(struct message* message, char* header_key);
+
+#endif
