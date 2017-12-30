7f769ca45f2008cc80b34780f9f2e8b65ab7fa58
lera <valeriya.syomina@yandex.ru>
2017-12-30 13:55:37 +0300

    message header list changed

--- src/message_headers_list.c
+++ src/message_headers_list.c
@@ -1,5 +1,6 @@
 #include "message_headers_list.h"
 #include <stdlib.h>
+#include <string.h>
 
 struct message_headers_list* create_empty_message_headers_list()
 {
@@ -9,6 +10,39 @@ struct message_headers_list* create_empty_message_headers_list()
 	list->header = NULL;
 	list->next = NULL;
 	return list;
+}
+
+struct message_header* get_header_by_key(struct message_headers_list* list, char* key)
+{
+    if (!key)
+        return NULL;
+    if (!list)
+        return NULL;
+    struct message_headers_list* cur = list;
+    struct message_header* result = NULL;
+    int found = 0;
+    while (cur && !found)
+    {
+        if (strcmp(cur->header->key, key) == 0)
+        {
+            found = 1;
+            result = cur->header;
+        }
+        cur = cur->next;
+    }
+    return result;
+}
+
+struct message_headers_list* create_message_headers_list_item(struct message_header* header)
+{
+    if (!header)
+        return NULL;
+    struct message_headers_list* element = (struct message_headers_list*) malloc(sizeof(struct message_headers_list));
+    if (!element)
+        return NULL;
+    element->header = header;
+    element->next = NULL;
+    return element;
 }
 
 void free_message_headers_list(struct message_headers_list* message_headers_list)
@@ -31,18 +65,13 @@ void free_message_headers_list(struct message_headers_list* message_headers_list
 	}
 }
 
-struct message_headers_list* add_header_to_list(struct message_headers_list* list, struct message_header* header)
+struct message_headers_list* add_to_message_headers_list(struct message_headers_list* list, struct message_headers_list* item)
 {
-	if (!list)
-		return NULL;
-	if (!header)
-		return NULL;
-
-	struct message_headers_list* element = create_empty_message_headers_list();
-	if (!element)
-		return NULL;
-	element->header = header;
-	element->next = list;
-
-	return element;
+    if (!item)
+        return NULL;
+    if (!list)
+        return item;
+    item->next = list;
+    return item;
 }
+
--- src/message_headers_list.h
+++ src/message_headers_list.h
@@ -1,3 +1,6 @@
+#ifndef MESSAGE_HEADER_LIST_H
+#define MESSAGE_HEADER_LIST_H
+
 #include "message_header.h"
 
 struct message_headers_list 
@@ -8,4 +11,8 @@ struct message_headers_list
 
 struct message_headers_list* create_empty_message_headers_list();
 void free_message_headers_list(struct message_headers_list* message_headers_list);
-struct message_headers_list* add_header_to_list(struct message_headers_list* list, struct message_header* header);
+struct message_headers_list* add_to_message_headers_list(struct message_headers_list* list, struct message_headers_list* item);
+struct message_headers_list* create_message_headers_list_item(struct message_header* header);
+struct message_header* get_header_by_key(struct message_headers_list* list, char* key);
+
+#endif
