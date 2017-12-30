077a1fc060a3d61288dbe507b3bea4b0034685aa
lera <valeriya.syomina@yandex.ru>
2017-12-30 14:04:21 +0300

    start client script added

---
+++ start_client.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+new_maildir_path="/home/lera/Desktop/maildir/new/"
+cur_maildir_path="/home/lera/Desktop/maildir/cur/"
+logger_queue_name="/test_queue"
+
+./src/client $new_maildir_path $cur_maildir_path $logger_queue_name
+
