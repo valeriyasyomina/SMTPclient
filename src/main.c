fe4fbab92c54d4a6de9b56498a023e8e48690a83
lera <valeriya.syomina@yandex.ru>
2017-12-30 14:05:27 +0300

    args added

--- src/main.c
+++ src/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 #include "smtp_client.h"
 
-int main()
+#define ARGS_NUMBER 4
+#define ERROR_ARGS_NUMBER -1
+
+int main(int argc, char** argv)
 {
-  //  printf("hello");
-    int res = start_smtp_client("/home/lera/Desktop/maildir/new/", "/home/lera/Desktop/maildir/cur/");
+    // "/home/lera/Desktop/maildir/new/", "/home/lera/Desktop/maildir/cur/", "/test_queue"
+    if (argc != ARGS_NUMBER)
+    {
+        printf("Usage: ./client <new_dir_path> <cur_dir_path> <logger_queue_name>");
+        return ERROR_ARGS_NUMBER;
+    }
+    int logger_flags = O_WRONLY;
+    int res = start_smtp_client(argv[1], argv[2], argv[3], logger_flags);
     printf("SMTP client terminated with code = %d\n", res);
-	return 0;
+    return res;
 }
