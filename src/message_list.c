d950e31ef4b73ecca8451802b0caf7c1102474cc
lera <valeriya.syomina@yandex.ru>
2017-12-30 13:55:20 +0300

    message list changed

--- src/message_list.c
+++ src/message_list.c
@@ -1,1 +1,69 @@
+#include "message_list.h"
 
+struct message_list* create_message_list_item(struct message* message)
+{
+    if (!message)
+        return NULL;
+    struct message_list* element = (struct message_list*) malloc(sizeof(struct message_list));
+    if (!element)
+        return NULL;
+    element->message = message;
+    element->next = NULL;
+    return element;
+}
+
+void free_message_list(struct message_list* list)
+{
+    if (list)
+    {
+        struct message_list* cur = NULL;
+        for (; list; list = cur)
+        {
+            cur = list->next;
+            if (list->message)
+            {
+                free_message(list->message);
+                list->message = NULL;
+            }
+            free(list);
+            list = NULL;
+        }
+        list = NULL;
+    }
+}
+
+struct message* get_message_by_id(struct message_list* list, char* id)
+{
+    if (!list || !id)
+        return NULL;
+    struct message_list* cur = list;
+    struct message* result =  NULL;
+    int message_found = 0;
+    while (cur && !message_found)
+    {
+        if (compare_message_id(cur->message, id) == 0)
+        {
+            result = cur->message;
+            message_found = 1;
+        }
+        cur = cur->next;
+    }
+    return result;
+}
+
+struct message_list* add_to_message_list(struct message_list* list, struct message_list* element)
+{
+    if (!element)
+        return NULL;
+    if (!list)
+        return element;
+    element->next = list;
+    return element;
+}
+
+struct message* get_message_by_header_key(struct message_list* list, char* header_key)
+{
+    if (!list || !list->message || !header_key)
+        return NULL;
+
+}
--- src/message_list.h
+++ src/message_list.h
@@ -1,5 +1,20 @@
 #ifndef MESSAGE_LIST_H
 #define MESSAGE_LIST_H
 
+#include "message.h"
+#include <stdlib.h>
+
+struct message_list
+{
+    struct message* message;
+    struct message_list* next;
+};
+
+struct message_list* create_message_list_item(struct message* message);
+void free_message_list(struct message_list* list);
+struct message_list* add_to_message_list(struct message_list* list, struct message_list* element);
+struct message* get_message_by_header_key(struct message_list* list, char* header_key);
+struct message* get_message_by_id(struct message_list* list, char* id);
+
 #endif // MESSAGE_LIST_H
 
