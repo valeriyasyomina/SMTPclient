98ca4a14361164e259ccc192026201c91ecc6e01
lera <valeriya.syomina@yandex.ru>
2017-12-30 13:54:45 +0300

    msg header struct changed

--- src/message_header.c
+++ src/message_header.c
@@ -29,6 +29,38 @@ struct message_header* create_header(char* key, char* value)
 	return header;
 }
 
+struct message_header* message_header_from_string(char* str)
+{
+    if (!str)
+        return NULL;
+    char* tmp = str;
+    while (*tmp != HEADER_DELIMITER)
+        tmp++;
+    int header_key_lenght = tmp - str;
+    char* key = (char*) malloc(header_key_lenght + 1);
+    strncpy(key, str, header_key_lenght);
+    key[header_key_lenght] = '\0';
+
+    tmp++;
+
+    while (*tmp == SPACE)
+        tmp++;
+
+    int header_value_length = strlen(str) - (tmp - str);
+    char* value = (char*) malloc(header_value_length + 1);
+    strncpy(value, tmp, header_value_length);
+    value[header_value_length] = '\0';
+
+    struct message_header* message_header = create_header(key, value);
+    free(key);
+    key = NULL;
+    free(value);
+    value = NULL;
+
+    return message_header;
+
+}
+
 void free_message_header(struct message_header* message_header)
 {
 	if (message_header)
@@ -45,4 +77,25 @@ void free_message_header(struct message_header* message_header)
 		}
 		message_header = NULL;
 	}
+}
+
+char* get_domain_name(struct message_header* header)
+{
+    if (!header || !header->key || !header->value)
+        return NULL;
+    if (strcmp(header->key, HEADER_TO) != 0)
+        return NULL;
+    char* tmp = header->value;
+    while (*(tmp++) != DOMAIN_DELIMITER)
+        ;
+    int domain_length = strlen(header->value) - (header->value - tmp);
+    char* domain = (char*) malloc(domain_length + 1);
+    strncpy(domain, tmp, domain_length);
+    domain[domain_length] = '\0';
+    return domain;
+}
+
+int compare_message_header_key(struct message_header* header, char* key)
+{
+    return strcmp(header->key, key);
 }
--- src/message_header.h
+++ src/message_header.h
@@ -1,3 +1,14 @@
+#ifndef MESSAGE_HEADER_H
+#define MESSAGE_HEADER_H
+
+#define HEADER_DELIMITER ':'
+#define DOMAIN_DELIMITER '@'
+#define SPACE ' '
+
+#define HEADER_FROM "BabarykinSyomina-SERVER_FROM"
+#define HEADER_TO "BabarykinSyomina-SERVER_TO"
+#define HEADER_MESSAGE_ID "BabarykinSyomina-SERVER_MESSAGE_ID"
+
 struct message_header
 {
 	char* key;
@@ -6,4 +17,9 @@ struct message_header
 
 struct message_header* create_header(char* key, char* value);
 void free_message_header(struct message_header* message_header);
+struct message_header* message_header_from_string(char* str);
 
+char* get_domain_name(struct message_header* header);
+int compare_message_header_key(struct message_header* header, char* key);
+
+#endif
