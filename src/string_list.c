60e86383d0c8707a870620ba2ee8318225a2f68d
lera <valeriya.syomina@yandex.ru>
2017-12-30 13:56:47 +0300

    string list changed

--- src/string_list.c
+++ src/string_list.c
@@ -1,51 +1,67 @@
 #include "string_list.h"
 
-struct string_list* create_empty_string_list()
+
+struct string_list* create_string_list_item(char* value)
 {
-	struct string_list* list = (struct string_list*) malloc(sizeof(struct string_list));
-	if (!list)
-		return NULL;
-	list->value = NULL;
-	list->next = NULL;
-	return list;
+    if (!value)
+        return NULL;
+    struct string_list* item = (struct string_list*) malloc(sizeof(struct string_list));
+    if (!item)
+        return NULL;
+
+    int value_size = strlen(value);
+    item->value = (char*) malloc(value_size + 1);
+    if (!item->value)
+        return NULL;
+    strncpy(item->value, value, value_size);
+    item->value[value_size] = '\0';
+
+    item->next = NULL;
+    return item;
 }
 
 void free_string_list(struct string_list* list)
 {
 	if (list)
 	{
-		struct string_list* cur = list;
-		while (cur)
-		{
-			cur = list->next;
+        struct string_list* cur = NULL;
+        for (; list; list = cur)
+        {
+            cur = list->next;
             if (list->value)
             {
                 free(list->value);
                 list->value = NULL;
             }
-			free(list);
-			list = cur;
-		}
-		list = NULL;
+            free(list);
+            list = NULL;
+        }
+        list = NULL;
 	}
 }
 
-struct string_list* add_string_to_list(struct string_list* list, char* value)
-{
-	if (!list)
+struct string_list* add_to_string_list(struct string_list* list, struct string_list* element)
+{	
+    if (!element)
 		return NULL;
-	if (!value)
-		return NULL;
-	struct string_list* element = create_empty_string_list();
-	if (!element)
-		return NULL;
-
-    int value_size = strlen(value);
-    element->value = (char*) malloc(value_size + 1);
-    if (!element->value)
-        return NULL;
-    strncpy(element->value, value, value_size);
-    element->value[value_size] = '\0';
-	element->next = list;
+    if (!list)
+        return element;
+    element->next = list;
 	return element;
+}
+
+void print_string_list(struct string_list* list)
+{
+    if (list)
+    {
+        struct string_list* cur = list;
+        printf("String list:\n");
+        while (cur)
+        {
+            if (cur->value)
+                printf("%s\n", cur->value);
+            cur = cur->next;
+        }
+        printf("\n\n");
+    }
 }
--- src/string_list.h
+++ src/string_list.h
@@ -1,5 +1,9 @@
+#ifndef STRING_LIST_H
+#define STRING_LIST_H
+
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 
 struct string_list 
 {
@@ -7,6 +11,9 @@ struct string_list
 	struct string_list* next;
 };
 
-struct string_list* create_empty_string_list();
+struct string_list* create_string_list_item(char* value);
 void free_string_list(struct string_list* list);
-struct string_list* add_string_to_list(struct string_list* list, char* value);
+struct string_list* add_to_string_list(struct string_list* list, struct string_list* element);
+void print_string_list(struct string_list* list);
+
+#endif
