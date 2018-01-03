0039c22365894d429782decd5e016c0df37a9fdc
lera <valeriya.syomina@yandex.ru>
2018-01-03 17:21:25 +0300

    sctipt params changed

--- start_client_valgrind.sh
+++ start_client_valgrind.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-new_maildir_path="/home/lera/Desktop/maildir/new/"
-cur_maildir_path="/home/lera/Desktop/maildir/cur/"
+new_maildir_path="./maildir/new/"
+cur_maildir_path="./maildir/cur/"
 logger_queue_name="/test_queue"
+attemps_number=3
+attermps_delay=1
 
 valgrind --leak-check=full --show-leak-kinds=all ./src/client $new_maildir_path $cur_maildir_path $logger_queue_name
 
