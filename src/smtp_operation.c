08547571abbcf2e5c23f8c982a0958146c002f38
lera <valeriya.syomina@yandex.ru>
2018-01-03 16:38:49 +0300

    read server commands added

--- src/smtp_operation.c
+++ src/smtp_operation.c
@@ -1,7 +1,7 @@
 #include "smtp_operation.h"
 
 
-int send_messages(struct domain_info_list* domains, mqd_t mq)
+int send_messages(struct domain_info_list* domains, mqd_t mq, int attempts_number, int attempts_delay)
 {
     if (!domains)
     {
@@ -15,7 +15,7 @@ int send_messages(struct domain_info_list* domains, mqd_t mq)
     struct domain_info_list* cur = domains;
     while (cur && result >= 0)
     {        
-        result = send_messages_one_domain(cur->info, mq);
+        result = send_messages_one_domain(cur->info, mq, attempts_number, attempts_delay);
         cur = cur->next;
     }
     if (result <= 0)
@@ -25,43 +25,45 @@ int send_messages(struct domain_info_list* domains, mqd_t mq)
     return result;
 }
 
-int send_messages_one_domain(struct domain_info* domain, mqd_t mq)
+int send_messages_one_domain(struct domain_info* domain, mqd_t mq, int attempts_number, int attempts_delay)
 {
     int result = 0;
     if (!domain)
     {
         send_log_message(mq, ERROR_LEVEL, "Null input domain");
         return NULL_DATA;
-    }
-
-    char buffer[MAX_RESPONCE_BUFFER_SIZE];
+    }    
 
     char log_message[MAX_LOGGER_MESSAGE_SIZE];
     snprintf(log_message, MAX_LOGGER_MESSAGE_SIZE, "Sending messages to domain %s", domain->domain_name);
     send_log_message(mq, DEBUG_LEVEL, log_message);
 
     send_log_message(mq, DEBUG_LEVEL, "Connecting to SMTP server...");
-    int smtp_socket_descriptor = try_connect_to_smtp_server(domain->domain_name);
+    int smtp_socket_descriptor = try_connect_to_smtp_server(domain->domain_name, attempts_number, attempts_delay);
     if (!smtp_socket_descriptor)
     {
         send_log_message(mq, ERROR_LEVEL, "Fail connect to SMTP server");
         return SOCKET_ERROR;
     }
+    char buffer[MAX_RESPONCE_BUFFER_SIZE];
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    int result_code = strtol(buffer, NULL, 10);
+
     send_log_message(mq, DEBUG_LEVEL, "Connection successufully established");
     send_log_message(mq, DEBUG_LEVEL, "Sending EHLO command");
-    result = send_command_to_smtp_server(smtp_socket_descriptor, EHLO_COMMAND_NUMBER, MY_SERVER_NAME);
-    if (result <= 0)
+
+    send_command_to_smtp_server(smtp_socket_descriptor, EHLO_COMMAND_NUMBER, MY_SERVER_NAME);
+
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    result_code = strtol(buffer, NULL, 10);
+    if (result_code != SUCCESS_OPERATION_CODE)
     {
         send_log_message(mq, ERROR_LEVEL, "Send EHLO command");
-        return ERROR_SEND_DATA;
+        return ERROR_RECEIVE_DATA;
     }
     send_log_message(mq, DEBUG_LEVEL, "EHLO command successufully send");
-
-   /* int responce_result = read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
-    if (responce_result < 0)
-        return ERROR_RECEIVE_DATA;
-    printf("SMTP server hello = %s\n", buffer); */
-
 
     result = send_domain_messages_list(smtp_socket_descriptor, domain->messages, mq);
     if (result <= 0)
@@ -72,18 +74,18 @@ int send_messages_one_domain(struct domain_info* domain, mqd_t mq)
     send_log_message(mq, DEBUG_LEVEL, "Domain's messages' successufully send");
 
     send_log_message(mq, DEBUG_LEVEL, "Sending QUIT command");
-    result = send_command_to_smtp_server(smtp_socket_descriptor, QUIT_COMMAND_NUMBER, NULL);
-    if (result <= 0)
+    send_command_to_smtp_server(smtp_socket_descriptor, QUIT_COMMAND_NUMBER, NULL);
+
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    result_code = strtol(buffer, NULL, 10);
+    if (result_code != SUCCESS_QUIT_CODE)
     {
         send_log_message(mq, ERROR_LEVEL, "Send QUIT command");
-        return ERROR_SEND_DATA;
+        return ERROR_RECEIVE_DATA;
     }
     send_log_message(mq, DEBUG_LEVEL, "QUIT command successufully send");
-
-
-   // read(smtp_socket_descriptor, buffer, 1024);
-   // printf("SMTP server quit = %s\n", buffer);
-
+    close(smtp_socket_descriptor);
     return result;
 }
 
@@ -115,50 +117,50 @@ int send_domain_message(int smtp_socket_descriptor, struct message* message, mqd
     struct message_header* from_header = get_message_header_by_key(message, HEADER_FROM);
 
     send_log_message(mq, DEBUG_LEVEL, "Send MAIL FROM command");
-    result = send_command_to_smtp_server(smtp_socket_descriptor, MAIL_FROM_COMMAND_NUMBER, from_header->value);
-    if (result < 0)
+    send_command_to_smtp_server(smtp_socket_descriptor, MAIL_FROM_COMMAND_NUMBER, from_header->value);
+
+    char buffer[MAX_RESPONCE_BUFFER_SIZE];
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    int responce_code = strtol(buffer, NULL, 10);
+    if (responce_code != SUCCESS_OPERATION_CODE)
     {
         send_log_message(mq, ERROR_LEVEL, "Send MAIL FROM command");
-        return ERROR_SEND_DATA;
-    }
-    send_log_message(mq, DEBUG_LEVEL, "MAIL FROM command successufully send");
-
-  /*  char buffer[MAX_RESPONCE_BUFFER_SIZE];
-
-    int responce_result = read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
-    if (responce_result < 0)
         return ERROR_RECEIVE_DATA;
-    printf("SMTP server mail from = %s\n", buffer); */
+    }
+
+    send_log_message(mq, DEBUG_LEVEL, "MAIL FROM command successufully send");
 
     result = process_mail_to_headers(smtp_socket_descriptor, message->headers, mq);
 
     send_log_message(mq, DEBUG_LEVEL, "Send DATA command");
-    result = send_command_to_smtp_server(smtp_socket_descriptor, DATA_COMMAND_NUMBER, NULL);
-    if (result <= 0)
+    send_command_to_smtp_server(smtp_socket_descriptor, DATA_COMMAND_NUMBER, NULL);
+
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    responce_code = strtol(buffer, NULL, 10);
+    if (responce_code != START_SEND_MSG_CODE)
     {
         send_log_message(mq, ERROR_LEVEL, "Send DATA command");
-        return ERROR_SEND_DATA;
+        return ERROR_RECEIVE_DATA;
     }
     send_log_message(mq, DEBUG_LEVEL, "DATA command successufully send");
-
-    /* responce_result = read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
-    if (responce_result < 0)
-        return ERROR_RECEIVE_DATA;
-    printf("SMTP server data com = %s\n", buffer); */
-
     send_log_message(mq, DEBUG_LEVEL, "Send message body");
-    result = send_command_to_smtp_server(smtp_socket_descriptor, MESSAGE_BODY_COMMAND_NUMBER, message->data);
-    if (result <= 0)
+
+    send_command_to_smtp_server(smtp_socket_descriptor, MESSAGE_BODY_COMMAND_NUMBER, message->data);
+
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    responce_code = strtol(buffer, NULL, 10);
+    if (responce_code != SUCCESS_OPERATION_CODE)
     {
         send_log_message(mq, ERROR_LEVEL, "Send message body");
-        return ERROR_SEND_DATA;
-    }
-    send_log_message(mq, DEBUG_LEVEL, "Message successufully send");
-
-   /* responce_result = read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
-    if (responce_result < 0)
         return ERROR_RECEIVE_DATA;
-    printf("SMTP server meessge = %s\n", buffer); */
+    }
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    responce_code = strtol(buffer, NULL, 10);
+    send_log_message(mq, DEBUG_LEVEL, "Message successufully send");
     return result;
 }
 
@@ -190,12 +192,14 @@ int send_mail_to_command(int smtp_socket_descriptor, struct message_header* to_h
     }
     send_log_message(mq, DEBUG_LEVEL, "RCPT TO command successufully send");
 
- /*   char buffer[MAX_RESPONCE_BUFFER_SIZE];
-
-    int responce_result = read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
-    if (responce_result < 0)
+    char buffer[MAX_RESPONCE_BUFFER_SIZE];
+    bzero(buffer, MAX_RESPONCE_BUFFER_SIZE);
+    read(smtp_socket_descriptor, buffer, MAX_RESPONCE_BUFFER_SIZE);
+    int respoce_code = strtol(buffer, NULL, 10);
+    if (respoce_code != SUCCESS_OPERATION_CODE)
+    {
         return ERROR_RECEIVE_DATA;
-    printf("SMTP server mail to = %s\n", buffer); */
+    }
     return result;
 }
 
@@ -231,23 +235,64 @@ int send_command_to_smtp_server(int socket_descriptor, int command_type, char* c
 }
 
 
-int try_connect_to_smtp_server(char* domain_name)
+int try_connect_to_smtp_server(char* domain_name, int attempts_number, int attempts_delay)
 {
     struct firedns_mxlist* xmlist = firedns_resolvemxlist_r(domain_name);
     int connection_stablished = 0;
     int smtp_socket = SOCKET_ERROR;
-    while (xmlist && !connection_stablished)
+    struct firedns_mxlist* cur = xmlist;
+    while (cur && !connection_stablished)
     {
-        printf("Server name %s\n", xmlist->name);
-        smtp_socket = create_socket(xmlist->name, SMTP_SERVER_PORT);
+        printf("Server name %s\n", cur->name);
+        smtp_socket = create_socket(cur->name, SMTP_SERVER_PORT, attempts_number, attempts_delay);
         if (smtp_socket > 0)
             connection_stablished = 1;
-        xmlist = xmlist->next;
+        cur = cur->next;
     }
+    free_xmlist(xmlist);
+    xmlist = NULL;
     return smtp_socket;
 }
 
-int create_socket(const char* host, int port)
+void free_xmlist(struct firedns_mxlist* xmlist)
+{
+    struct firedns_mxlist* cur = xmlist;
+
+    while (cur)
+    {
+        cur = xmlist->next;
+      /*  if (xmlist->ip4list)
+            free_firedns_ip4list(xmlist->ip4list);
+        if (xmlist->ip6list)
+            free_firedns_ip6list(xmlist->ip6list); */
+        free(xmlist);
+        xmlist = cur;
+    }
+}
+
+void free_firedns_ip6list(struct firedns_ip6list* list)
+{
+    struct firedns_ip6list *ip4_cur = list;
+    while (ip4_cur)
+    {
+        ip4_cur = list->next;
+        free(list);
+        list = ip4_cur;
+    }
+}
+
+void free_firedns_ip4list(struct firedns_ip4list* list)
+{
+    struct firedns_ip4list *ip4_cur = list;
+    while (ip4_cur)
+    {
+        ip4_cur = list->next;
+        free(list);
+        list = ip4_cur;
+    }
+}
+
+int create_socket(const char* host, int port, int attempts_number, int attempts_delay)
 {
     struct hostent *he;
     struct in_addr **addr_list;
@@ -267,13 +312,35 @@ int create_socket(const char* host, int port)
         server.sin_addr.s_addr = inet_addr(inet_ntoa(*addr_list[i]));
         server.sin_port = htons(SMTP_SERVER_PORT);
 
-        int socket_descriptor = socket(AF_INET , SOCK_STREAM , 0);
-        int connect_result = connect(socket_descriptor,(struct sockaddr *)&server, sizeof(server));
-        if (connect_result == 0)
+      /*  int socket_descriptor = socket(AF_INET , SOCK_STREAM , 0);
+        int connect_result = connect(socket_descriptor,(struct sockaddr *)&server, sizeof(server)); */
+        int socket_descriptor = try_connect_to_socket(server, attempts_number, attempts_delay);
+        if (socket_descriptor > 0)
         {
             socket_found = 1;
             result = socket_descriptor;
         }
+    }
+    return result;
+}
+
+int try_connect_to_socket(struct sockaddr_in server, int attempts_number, int attempts_delay)
+{
+    int socket_descriptor = socket(AF_INET , SOCK_STREAM , 0);
+    int i = 0;
+    int result = SOCKET_ERROR;
+    int connection_established = 0;
+    while (i < attempts_number && !connection_established)
+    {
+        int connect_result = connect(socket_descriptor,(struct sockaddr *)&server, sizeof(server));
+        if (connect_result == 0)
+        {
+            connection_established = 1;
+            result = socket_descriptor;
+        }
+        else
+            sleep(attempts_delay);
+        i++;
     }
     return result;
 }
