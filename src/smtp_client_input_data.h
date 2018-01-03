c3a1aea218702008055d7e4d6f2818aa4429b4bd
lera <valeriya.syomina@yandex.ru>
2018-01-03 16:39:49 +0300

    input data struct added

--- src/smtp_client_input_data.h
+++ src/smtp_client_input_data.h
@@ -1,5 +1,15 @@
 #ifndef SMTP_CLIENT_INPUT_DATA
 #define SMTP_CLIENT_INPUT_DATA
 
+struct smtp_client_input_data
+{
+    char* new_directory_name;
+    char* cur_directory_name;
+    int attempts_number;
+    int attempts_delay;
+    char* logger_fifo_name;
+    int logger_flags;
+};
+
 #endif // SMTP_CLIENT_INPUT_DATA
 
