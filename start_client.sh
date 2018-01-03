672a41d321cd6add20923a85ee1f616ffffdb3ca
lera <valeriya.syomina@yandex.ru>
2018-01-03 16:42:22 +0300

    extra params added

--- start_client.sh
+++ start_client.sh
@@ -3,6 +3,8 @@
 new_maildir_path="/home/lera/Desktop/maildir/new/"
 cur_maildir_path="/home/lera/Desktop/maildir/cur/"
 logger_queue_name="/test_queue"
+attemps_number=3
+attermps_delay=1
 
-./src/client $new_maildir_path $cur_maildir_path $logger_queue_name
+./src/client $new_maildir_path $cur_maildir_path $logger_queue_name $attemps_number $attermps_delay
 
